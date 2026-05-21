ae493c4d25cd28e6deca02032a73dcaacbc8dd1a
Regina Liu <regina.liu@techfi.io>
2026-05-21 16:45:01 +0800

    fix on README file

--- dbt_stocks/models/marts/fct_stock_indicators_unpivoted.sql
+++ dbt_stocks/models/marts/fct_stock_indicators_unpivoted.sql
@@ -1,30 +1,31 @@
 -- fct_stock_indicators_unpivoted.sql
-{{ config(materialized='view') }}
+{{ config(materialized="view") }}
 
-with base as (
-    select
-        symbol,
-        quote_date,
-        'close_price' as indicator,
-        close_price_validated as value,
+with
+    base as (
+        select
+            symbol,
+            quote_date,
+            'close_price' as indicator,
+            close_price_validated as value
 
-    from {{ ref('fct_stock_history_performance') }}
+        from {{ ref("fct_stock_history_performance") }}
 
-    union all
+        union all
 
-    select symbol, quote_date, 'SMA 20', sma_20
-    from {{ ref('fct_stock_history_performance') }}
+        select symbol, quote_date, 'SMA 20', sma_20
+        from {{ ref("fct_stock_history_performance") }}
 
-    union all
+        union all
 
-    select symbol, quote_date, 'SMA 50', sma_50
-    from {{ ref('fct_stock_history_performance') }}
+        select symbol, quote_date, 'SMA 50', sma_50
+        from {{ ref("fct_stock_history_performance") }}
 
-    union all
+        union all
 
-    select symbol, quote_date, 'SMA 200', sma_200
-    from {{ ref('fct_stock_history_performance') }}
-)
+        select symbol, quote_date, 'SMA 200', sma_200
+        from {{ ref("fct_stock_history_performance") }}
+    )
 
 select *
 from base
